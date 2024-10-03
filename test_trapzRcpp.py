bf7e0c28b9f7265406b00fd0b0ff5f68e4a202b8
Rithvik Donnipadu <rkdonnnipadu@dons.usfca.edu>
2024-10-03 18:16:16 -0500

    Modified trapzRcpp.cpp

--- test_trapzRcpp.py
+++ test_trapzRcpp.py
@@ -8,9 +8,9 @@ from trapzRcpp import trapz
 def test_trapzRcpp_trivial_example():
     x = np.array([0, 2])
     y = np.array([0, 2])
-    assert np.isclose(trapzRcpp(x, y), 2)
+    assert np.isclose(trapz(x, y), 2)
 
 def test_trapzRcpp_nearly_trivial_example():
     x = np.linspace(0, 4, 100)
     y = x + np.sin(x)
-    assert np.isclose(trapzRcpp(x, y), 9.653418652171286)
+    assert np.isclose(trapz(x, y), 9.653418652171286)
